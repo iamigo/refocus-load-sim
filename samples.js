c578138ca143e9658582025db5e22004659c6b31
Ian Goldstein <igoldstein@salesforce.com>
2017-02-08 19:03:51 -0800

    Adds related links to samples

--- samples.js
+++ samples.js
@@ -8,6 +8,13 @@ const rconf = require('./config/refocus');
 const rc = new RefocusClient(rconf.refocusUrl, rconf.apiVersion, rconf.token);
 const rand = new Random();
 
+const rl = [
+  { name: 'Google', url: 'http://www.google.com' },
+  { name: 'Yahoo', url: 'http://www.yahoo.com' },
+  { name: 'Salesforce', url: 'http://www.salesforce.com' },
+  { name: 'Refocus', url: 'https://github.com/salesforce/refocus' },
+];
+
 function upsert(roots) {
   const batches = {};
   Object.keys(roots).forEach((r) => {
@@ -17,6 +24,7 @@ function upsert(roots) {
         batches[r].push({
           name: `${s}|${a}`,
           value: (rand.real(-1, 101)).toString(),
+          relatedLinks: rl,
         });
       });
     });
